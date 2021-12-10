9bd6ef8187abcda37b6142b41e7325e3116c802f
ohmink <kkop5056@naver.com>
2021-12-11 04:01:57 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "KOM's Blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
