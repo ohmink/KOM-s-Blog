de63313828ddb904dc4910128cd0d5dcc42ff3f3
ohmink <kkop5056@naver.com>
2021-12-12 00:15:54 +0900

    set gatsby-source-filesystem(blog data)

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,18 +7,25 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "whatisthis?",
       },
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-mdx",
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "pages",
+    //     path: "./src/pages/",
+    //   },
+    //   __key: "pages",
+    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
-      __key: "pages",
     },
   ],
 };
