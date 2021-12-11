dd69dba3f9397d71a467669662ea80b2903e276f
ohmink <kkop5056@naver.com>
2021-12-12 04:21:43 +0900

    Create header

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { header } from "./header.module.css";
+
+const Header = () => {
+  return <header className={header}></header>;
+};
+
+export default Header;
