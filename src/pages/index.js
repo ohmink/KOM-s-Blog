ab61bcd8c5321e3b8d73f588a393896ebb6417cb
ohmink <kkop5056@naver.com>
2021-12-12 04:25:52 +0900

    Fix with component reuse

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,10 @@
 import * as React from "react";
-import { indexPage, indexContents } from "./index.module.css";
-import { MDXRenderer } from "gatsby-plugin-mdx";
 import { graphql } from "gatsby";
-import SideBar from "../components/sidebar";
+import BasicLayout from "../components/basic-layout";
 
-const IndexPage = ({ data }) => {
-  return (
-    <main className={indexPage}>
-      <SideBar />
-      <div className={indexContents}>
-        <title>Home Page</title>
-        <h1>Welcome to my Gatsby site!</h1>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-
-        <ul>
-          {data.allMdx.nodes.map((node) => (
-            <article key={node.id}>
-              <p>{node.frontmatter.title}</p>
-              <p>{node.frontmatter.date}</p>
-              <MDXRenderer>{node.body}</MDXRenderer>
-            </article>
-          ))}
-        </ul>
-      </div>
-    </main>
-  );
+const BlogPage = ({ data }) => {
+  return <BasicLayout data={data} />;
 };
-
-export default IndexPage;
 
 export const query = graphql`
   query MyQuery {
@@ -36,10 +13,14 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          summary
         }
         id
         body
+        slug
       }
     }
   }
 `;
+
+export default BlogPage;
