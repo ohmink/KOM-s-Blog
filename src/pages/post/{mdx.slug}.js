bb135885fae47143e51a5a5c89461908018759be
ohmink <kkop5056@naver.com>
2021-12-14 06:17:05 +0900

    Create post page & Update style

--- src/pages/post/{mdx.slug}.js
+++ src/pages/post/{mdx.slug}.js
@@ -1,15 +1,31 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { postTitle, postDate } from "./{mdx.slog}.module.css";
+import {
+  post,
+  postTitle,
+  postInfo,
+  postInfoTags,
+  postInfoDate,
+} from "./{mdx.slog}.module.css";
 import BasicLayout from "../../components/basic-layout";
 
 const BlogPost = ({ data }) => {
   return (
-    <BasicLayout originData={null} setPostList={null}>
-      <div>
-        <h2>{data.mdx.frontmatter.title}</h2>
-        <p>{data.mdx.frontmatter.date}</p>
+    <BasicLayout
+      title={data.mdx.frontmatter.title}
+      content={data.mdx.internal.content}
+    >
+      <div className={post}>
+        <h1 className={postTitle}>{data.mdx.frontmatter.title}</h1>
+        <div className={postInfo}>
+          <span className={postInfoTags}>
+            {data.mdx.frontmatter.tag.map((name) => (
+              <p>#{name}</p>
+            ))}
+          </span>
+          <p className={postInfoDate}>{data.mdx.frontmatter.date}</p>
+        </div>
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </div>
     </BasicLayout>
@@ -19,12 +35,15 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
-        summary
+        tag
       }
-      body
+      internal {
+        content
+      }
     }
   }
 `;
