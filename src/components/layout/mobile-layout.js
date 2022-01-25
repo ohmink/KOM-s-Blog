2169184279be168fe46ccd7ad23f6531ae1f69a2
ohmink <kkop5056@naver.com>
2022-01-26 01:53:32 +0900

    Add logic of dark mode

--- src/components/layout/mobile-layout.js
+++ src/components/layout/mobile-layout.js
@@ -2,8 +2,17 @@ import * as React from "react";
 import * as Styles from "./mobile-layout.module.css";
 import MobileHeader from "./mobile-items/mobile-header";
 import MobileNav from "./mobile-items/mobile-nav";
+import { themeStateContext } from "../themeProvider";
 
-const MobileLayout = ({ children, tags, selectedTag }) => {
+const MobileLayout = (props) => {
+  const { children, tags, selectedTag, GlobalStyles } = props;
+  const theme = React.useContext(themeStateContext);
+
+  React.useEffect(() => {
+    if (theme.mode === "dark") document.body.classList.add(GlobalStyles.dark);
+    else document.body.classList.remove(GlobalStyles.dark);
+  }, [theme.mode]);
+
   return (
     <div className={Styles.mobileLayout}>
       <MobileHeader />
