3d64f8412c9bdd143135805b444ce734d7fa5e05
ohmink <kkop5056@naver.com>
2022-01-28 20:28:00 +0900

    Update toggle animation and style

--- src/components/layout/layout-public/theme-toggle.js
+++ src/components/layout/layout-public/theme-toggle.js
@@ -1,18 +1,37 @@
 import * as React from "react";
 import * as Styles from "./theme-toggle.module.css";
+import { themeStateContext, themeDispatchContext } from "../../themeProvider";
 
 const ThemeToggle = () => {
+  const theme = React.useContext(themeStateContext);
+  const themeDispatch = React.useContext(themeDispatchContext);
+  const toggleRef = React.useRef();
   const sunRef = React.useRef();
   const moonRef = React.useRef();
 
-  function toggle() {
-    console.log("test");
-    sunRef.current.classList.toggle(Styles.animateSun);
-    moonRef.current.classList.toggle(Styles.animateMoon);
+  function setLogo() {
+    if (theme.mode === "dark") {
+      sunRef.current.classList.toggle(Styles.animateSun);
+      moonRef.current.classList.toggle(Styles.animateMoon);
+    }
   }
 
+  function toggle() {
+    setLogo();
+
+    if (theme.mode === "light") themeDispatch({ type: "dark" });
+    else themeDispatch({ type: "light" });
+  }
+
+  React.useEffect(() => {
+    toggleRef.current.style.backgroundColor = theme.colors.backgroundColor;
+    toggleRef.current.style.boxShadow = theme.colors.boxShadow;
+
+    setLogo();
+  }, [theme.mode]);
+
   return (
-    <div className={Styles.toggleContainer} onClick={toggle}>
+    <div ref={toggleRef} className={Styles.toggleContainer} onClick={toggle}>
       <div ref={sunRef} className={`${Styles.sun} ${Styles.sunLogo}`}>
         <svg viewBox="0 0 512 512" width={20} title="sun">
           <path d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z" />
